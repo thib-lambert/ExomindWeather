5985788347f1e5b3afe3b11c8e5717c5bfb11e6a
Thibaud Lambert <tlambert@exomind.fr>
2021-06-04 15:05:34 +0200

    [App] Add colors

---
+++ ExomindWeather/Helpers/Extensions/UIColor+Colors.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+Colors.swift
+//  ExomindWeather
+//
+//  Created by Thibaud Lambert on 04/06/2021.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let celadonBlue = UIColor(named: "CeldonBlue")
+    static let davysGrey = UIColor(named: "DavysGrey")
+    static let fireOpal = UIColor(named: "FireOpal")
+    static let naplesYellow = UIColor(named: "NaplesYellow")
+}
